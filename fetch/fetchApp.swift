a61bdb69ce7c247366dd8f32672b86ee54127cdd
kehindeBankole <bankolek1@gmail.com>
2023-12-21 11:01:23 +0100

    Initial Commit

---
+++ fetch/fetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchApp.swift
+//  fetch
+//
+//  Created by kehinde on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct fetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
