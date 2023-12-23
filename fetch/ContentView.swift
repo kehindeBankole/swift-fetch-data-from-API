4d612e47a7b003fa2da9244f24b10fb1e4e95e9d
kehindeBankole <bankolek1@gmail.com>
2023-12-23 17:53:08 +0100

    fetch data from api

--- fetch/ContentView.swift
+++ fetch/ContentView.swift
@@ -6,17 +6,71 @@
 //
 
 import SwiftUI
+struct Post: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let body: String
+}
 
+enum MyError: Error {
+case badParsing
+case badRequest
+}
 struct ContentView: View {
+    @State private var posts: [Post] = []
+    @State private var isLoading = false
+    private func fetchData() async throws -> Any {
+        // Parse URL
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            throw MyError.badParsing
+        
+        }
+        
+        let request = URLRequest(url: url)
+        do {
+            isLoading = true
+            let (data, _) = try await URLSession.shared.data(for: request)
+            // Parse JSON
+            let decodedData = try JSONDecoder().decode([Post].self, from: data)
+            posts = decodedData
+            
+            isLoading = false
+            
+            return decodedData
+        } catch {
+            // Print errors
+          
+            throw MyError.badRequest
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if(isLoading){
+                ProgressView("Loading...")
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .padding()
+                
+            }else{
+                List(posts){post in
+                    Text(post.title).padding(.vertical,20).swipeActions(content: {
+                        Button(role: .destructive, action: {
+                            print(post)
+                        }, label: {
+                            
+                            Image(systemName: "trash.circle")
+                        })
+                    })
+                }.listStyle(.plain).listRowInsets(EdgeInsets()).scrollIndicators(.hidden)
+            }
+        }.task(priority: .background) {
+            
+            _ = try? await fetchData()
+            
         }
         .padding()
     }
+    
 }
 
 #Preview {
