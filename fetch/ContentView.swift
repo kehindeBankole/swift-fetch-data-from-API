239088857d33fb0ac6cde19fb7cb1e32630f0931
kehindeBankole <bankolek1@gmail.com>
2023-12-24 10:55:56 +0100

    fetch remote image

--- fetch/ContentView.swift
+++ fetch/ContentView.swift
@@ -31,11 +31,6 @@ struct ContentView: View {
   private let imageURL = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/564ce718-2b81-46ca-800d-fe48cc091e46/df9jywd-5c22ac40-b70c-46d0-a739-719f72c4ae64.jpg/v1/fill/w_1280,h_720,q_75,strp/satoru_gojo_jujutsu_kaisen_4k_pc_wallpaper_by_volt783_df9jywd-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNTY0Y2U3MTgtMmI4MS00NmNhLTgwMGQtZmU0OGNjMDkxZTQ2XC9kZjlqeXdkLTVjMjJhYzQwLWI3MGMtNDZkMC1hNzM5LTcxOWY3MmM0YWU2NC5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Nhd3-2XkRngJCHJbdeQIbCLJ1SgLfBJHTXFLhvSpMcQ"
     
     private func fetchData() async throws -> Any {
-        
-        for (index, elem) in  [1,3,4].enumerated() {
-          // statements
-            print(index , elem)
-        }
         // Parse URL
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
             throw MyError.badParsing
